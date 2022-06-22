f8b78ac270c49358b0798a95771cdd4a6a1cecdc
Bishwash Adhikari <bishwashadhikari1@gmail.com>
2022-06-22 19:48:37 +0545

    dashboard app initialized

--- webot/urls.py
+++ webot/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from dashboard.views import dashboard
+
 
 
 urlpatterns = [
@@ -23,5 +25,6 @@ urlpatterns = [
  
     path('', include('base.urls')),
     path('', include('preferances.urls')),
+    path('', include('dashboard.urls')),
 
 ]
