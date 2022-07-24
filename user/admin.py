3149915911b77f245af4dbb959e3aef20a36fa75
bishwashadhikari1 <bishwashadhikari1@gmail.com>
2022-07-24 23:50:27 +0545

    final changes maybe

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from subscription.models import Subscription, TransactionHistory
+from preferances.models import Preferances
 # Register your models here.
+
+admin.site.register(Subscription)
+admin.site.register(TransactionHistory)
+admin.site.register(Preferances)
+
+
